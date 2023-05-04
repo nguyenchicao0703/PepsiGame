70f1d34d7c2ef2886729122801997f7ef9f181cc
nguyenchicao0703 <chicao0703@gmail.com>
2023-05-04 16:36:31 +0700

    setup react-native
    setup react-native

---
+++ PepsiGame/App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color: 'black', fontSize: 50}}>App</Text>
+    </View>
+  )
+}
+
+export default App
